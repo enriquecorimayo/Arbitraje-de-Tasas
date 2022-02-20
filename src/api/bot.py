b2fd090434e400d636daa3709c1384f129700b19
enriquecorimayo <enriquecorimayo2011@gmail.com>
2022-02-20 13:25:05 -0300

    update bot.py

--- src/api/bot.py
+++ src/api/bot.py
@@ -3,10 +3,8 @@ from src.model import tradingbot as bot
 
 def main():
 
-    underlier_update_frecuency = 0.5
     tickers = ["PAMP", "YPFD", "GGAL", "DLR"]
-    bot.TradingBot(tickers, underlier_update_frecuency).start()
-
+    bot.TradingBot(tickers).start()
 
 if __name__ == "__main__":
     main()
